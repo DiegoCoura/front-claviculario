8db222d5c7b101d50b9ac4595b348a227fce0af1
DiegoCoura <diegogomes.10.9@gmail.com>
2024-05-07 20:03:37 -0300

    feat: navbar renders with the username

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
+import { useContext } from "react";
 import { NavContainer } from "./NavbarStyled";
+import { UserContext } from "../../Context/UserContext";
 
 export default function NavBar() {
+  const { user } = useContext(UserContext)
   return (
     <>
       <NavContainer>
-        <h3>Nome do usuário</h3>
+        <h3>{user.name}</h3>
         <button>
           <img src="/gear.svg"></img>
         </button>
