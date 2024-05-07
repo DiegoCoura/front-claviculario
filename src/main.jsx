c79f030950ba7c2ae2462f73f8725ab3082ff6f8
DiegoCoura <diegogomes.10.9@gmail.com>
2024-05-07 20:03:00 -0300

    feat: add routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,24 +5,45 @@ import { GlobalStyled } from "./GlobalStyled.jsx";
 import HomeUser from "./pages/User/Home/HomeUser.jsx";
 import ClassRooms from "./pages/User/ClassRooms/ClassRooms.jsx";
 import ClassRequests from "./pages/User/ClassRequests/ClassRequests.jsx";
+import UserProvider from "./Context/UserContext.jsx";
+import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
+import Authentication from "./pages/Authentication/Authentication.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/user",
     element: <HomeUser />,
-    children: [{
-      path: "/user/salas",
-      element: <ClassRooms />
-    },{
-      path: "/user/solicitacoes",
-      element: <ClassRequests />
-    }]
-  },
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/user/salas/todas",
+        element: <ClassRooms />,
+      },
+      {
+        path: "/user/salas/livres",
+        element: <h1>Livres</h1>,
+      },
+      {
+        path: "/user/salas/ocupadas",
+        element: <h1>Ocupadas</h1>,
+      },
+      {
+        path: "/user/solicitacoes",
+        element: <ClassRequests />,
+      },
+    ],
+  },{
+    path: "/login",
+    element: <Authentication />,
+    errorElement: <ErrorPage />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GlobalStyled />
-    <RouterProvider router={router} />
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
   </React.StrictMode>
 );
