bb6f9bdf0e10a415b4c52045611e4955166b8eb5
DiegoCoura <diegogomes.10.9@gmail.com>
2024-05-09 19:52:33 -0300

    feat: display the user classrooms

--- src/pages/User/ClassRooms/ClassRooms.jsx
+++ src/pages/User/ClassRooms/ClassRooms.jsx
@@ -1,7 +1,22 @@
-export default function ClassRooms () {
-    return(
-        <>
-            <h1>Minhas Salas</h1>
-        </>
-    )
+import { useContext } from "react";
+import { UserContext } from "../../../Context/UserContext";
+import { Users } from "../../../mock-data/Users";
+import { getUser } from "../../../utils/getUser";
+import { rooms } from "../../../mock-data/rooms";
+
+export default function ClassRooms() {
+  const { user } = useContext(UserContext);
+
+  const currUser = getUser(Users, user);
+
+  const currUsersRooms = currUser.classrooms.map((room) => {
+    let currRoom = rooms.find((roomDB) => roomDB.room === room);
+    return <div key={room}>{currRoom.room} / {currRoom.status}</div>;
+  });
+
+  return (
+    <>
+      {currUsersRooms}
+    </>
+  );
 }
