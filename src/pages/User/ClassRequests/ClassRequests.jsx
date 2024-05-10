3092ab5d15b0470c6db6c9fb9d9d7685d01b1823
DiegoCoura <diegogomes.10.9@gmail.com>
2024-05-09 21:18:54 -0300

    feat: ClassRequests component working with mock data

--- src/pages/User/ClassRequests/ClassRequests.jsx
+++ src/pages/User/ClassRequests/ClassRequests.jsx
@@ -1,7 +1,51 @@
-export default function ClassRequests (){
-    return(
-        <>
-            <h1>Solicitações</h1>
-        </>
-    )
+import { useState } from "react";
+import { getRoom } from "../../../utils/getFunctions";
+import { useForm } from "react-hook-form";
+import Input from "../../../components/Input/Input";
+import { roomsDB } from "../../../mock-data/rooms";
+import Button from "../../../components/Button/Button";
+import { ClassRequestContainer, RequestContainer, ResultContainer } from "./ClassRequestsStyled";
+
+export default function ClassRequests() {
+  const [currentRoom, setCurrentRoom] = useState("");
+  const {
+    register: registerSearch,
+    handleSubmit: handleSearch,
+    reset,
+  } = useForm();
+
+  const onSearch = (data) => {
+    const { room } = data;
+    const currRoom = getRoom(roomsDB, room.toUpperCase());
+    setCurrentRoom(currRoom);
+    reset()
+  };
+
+  return (
+    <>
+      <ClassRequestContainer>
+        <RequestContainer>
+          <form action="" onSubmit={handleSearch(onSearch)}>
+            <Input
+              type="text"
+              placeholder="SALA"
+              name="room"
+              register={registerSearch}
+            />
+            <Button type={"submit"} text={"Buscar Sala"}>
+              Solicitar Acesso
+            </Button>
+          </form>
+        </RequestContainer>
+        {currentRoom && (
+          <ResultContainer>
+            <div>
+              {currentRoom.room} {currentRoom.status}
+            </div>
+            <Button text={"solicitar Acesso"} type={"submit"} />
+          </ResultContainer>
+        )}
+      </ClassRequestContainer>
+    </>
+  );
 }
