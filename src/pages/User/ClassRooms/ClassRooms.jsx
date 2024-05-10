b65d4cc2aa0f97e25654e2466ff795614ba96a15
DiegoCoura <diegogomes.10.9@gmail.com>
2024-05-09 21:20:00 -0300

    change: rooms to roomsDB on ClassRooms component

--- src/pages/User/ClassRooms/ClassRooms.jsx
+++ src/pages/User/ClassRooms/ClassRooms.jsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
 import { UserContext } from "../../../Context/UserContext";
 import { Users } from "../../../mock-data/Users";
-import { getUser } from "../../../utils/getUser";
-import { rooms } from "../../../mock-data/rooms";
+import { getRoom, getUser } from "../../../utils/getFunctions";
+import { roomsDB } from "../../../mock-data/rooms";
 
 export default function ClassRooms() {
   const { user } = useContext(UserContext);
@@ -10,7 +10,7 @@ export default function ClassRooms() {
   const currUser = getUser(Users, user);
 
   const currUsersRooms = currUser.classrooms.map((room) => {
-    let currRoom = rooms.find((roomDB) => roomDB.room === room);
+    let currRoom = getRoom(roomsDB, room);
     return <div key={room}>{currRoom.room} / {currRoom.status}</div>;
   });
 
