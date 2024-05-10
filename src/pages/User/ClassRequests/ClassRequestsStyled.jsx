76156521d3ad2421a414039a9b76aeb09541e572
DiegoCoura <diegogomes.10.9@gmail.com>
2024-05-09 21:19:17 -0300

    feat: ClassRequests styled component

---
+++ src/pages/User/ClassRequests/ClassRequestsStyled.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const ClassRequestContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+`;
+
+export const RequestContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 1rem;
+  margin-top: 1rem;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  }
+`;
+
+export const ResultContainer = styled.div`
+  display: flex;
+  gap: 10px;
+`;
