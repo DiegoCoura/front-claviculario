59366820f8eaccb0eb99e5fb79c0e984e2885710
DiegoCoura <diegogomes.10.9@gmail.com>
2024-05-10 22:42:49 -0300

    change: user context using mock data

--- src/Context/UserContext.jsx
+++ src/Context/UserContext.jsx
@@ -1,12 +1,10 @@
 import { createContext, useState } from "react";
+import { Users } from "../mock-data/Users";
 
 export const UserContext = createContext();
 
 export default function UserProvider({ children }) {
-  const [user, setUser] = useState({
-    name: "Jussaro Cleber",
-    role: "student"
-  });
+  const [user, setUser] = useState(Users[0]);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
