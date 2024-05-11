e28a68c6fe0e23c078b32ad62c91bf56cb4f2fd9
DiegoCoura <diegogomes.10.9@gmail.com>
2024-05-10 22:41:52 -0300

    feat: Button component accepts a class name

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,9 +4,10 @@ export default function Button({
   type,
   text,
   handleClick,
+  className
 }) {
   return (
-    <ButtonSpace type={type} onClick={handleClick}>
+    <ButtonSpace className={className} type={type} onClick={handleClick}>
       {text}
     </ButtonSpace>
   );
