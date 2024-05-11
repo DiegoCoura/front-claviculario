54d3db0f24066a9a8d6a36adce216c911502604d
DiegoCoura <diegogomes.10.9@gmail.com>
2024-05-10 22:45:17 -0300

    feat: add conditional rendering for student and professor roles

--- src/pages/User/ClassRequests/ClassRequests.jsx
+++ src/pages/User/ClassRequests/ClassRequests.jsx
@@ -1,13 +1,23 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { getRoom } from "../../../utils/getFunctions";
 import { useForm } from "react-hook-form";
 import Input from "../../../components/Input/Input";
 import { roomsDB } from "../../../mock-data/rooms";
+import { roomRequests } from "../../../mock-data/roomsRequests";
 import Button from "../../../components/Button/Button";
-import { ClassRequestContainer, RequestContainer, ResultContainer } from "./ClassRequestsStyled";
+import {
+  ClassRequestContainer,
+  ListContainer,
+  RequestContainer,
+  ResultContainer,
+} from "./ClassRequestsStyled";
+import { UserContext } from "../../../Context/UserContext";
 
 export default function ClassRequests() {
+  const { user } = useContext(UserContext);
   const [currentRoom, setCurrentRoom] = useState("");
+  const [currentRequest, setCurrentRequest] = useState({});
+
   const {
     register: registerSearch,
     handleSubmit: handleSearch,
@@ -15,11 +25,53 @@ export default function ClassRequests() {
   } = useForm();
 
   const onSearch = (data) => {
+    if (!data.room) return;
     const { room } = data;
     const currRoom = getRoom(roomsDB, room.toUpperCase());
     setCurrentRoom(currRoom);
-    reset()
+    reset();
   };
+
+  const onRequest = () => {
+    let currRequest = {
+      room: currentRoom.room,
+      aluno: user.name,
+      phone: user.phone,
+      status: "pendente",
+    };
+    let dupleRequest = roomRequests.some(
+      (request) => request.room === currRequest.room
+    );
+    if (dupleRequest === true) return;
+    setCurrentRequest(currRequest);
+    roomRequests.push(currRequest);
+  };
+
+  let requestsList;
+  if (user.role === "student") {
+    requestsList = roomRequests.map((req) => {
+      return (
+        <div key={req.room}>
+          Sala: {req.room} - Status: {req.status}
+        </div>
+      );
+    });
+  } else if (user.role === "professor") {
+    requestsList = roomRequests.map((req) => {
+      return (
+        <li key={`${req.room}-${req.phone}`}>
+          <div>
+            Sala: {req.room} - {req.aluno} - cel: {req.phone} - Status:{" "}
+            {req.status}
+          </div>
+          <div>
+            <Button text={"Aprovar"} type={"button"}></Button>
+            <Button className={"red"} text={"Recusar"} type={"button"}></Button>
+          </div>
+        </li>
+      );
+    });
+  }
 
   return (
     <>
@@ -32,19 +84,20 @@ export default function ClassRequests() {
               name="room"
               register={registerSearch}
             />
-            <Button type={"submit"} text={"Buscar Sala"}>
-              Solicitar Acesso
-            </Button>
+            <Button type={"submit"} text={"Buscar Sala"} />
           </form>
         </RequestContainer>
         {currentRoom && (
           <ResultContainer>
-            <div>
-              {currentRoom.room} {currentRoom.status}
-            </div>
-            <Button text={"solicitar Acesso"} type={"submit"} />
+            <div>{currentRoom.room}</div>
+            <Button
+              type={"button"}
+              text={"solicitar Acesso"}
+              handleClick={() => onRequest()}
+            />
           </ResultContainer>
         )}
+        <ListContainer>{requestsList}</ListContainer>
       </ClassRequestContainer>
     </>
   );
