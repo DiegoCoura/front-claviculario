8fcdf895f0d224d3d0e75a5d606a2d28a8900e6f
DiegoCoura <diegogomes.10.9@gmail.com>
2024-05-10 22:46:16 -0300

    feat; add list container for the requests

--- src/pages/User/ClassRequests/ClassRequestsStyled.jsx
+++ src/pages/User/ClassRequests/ClassRequestsStyled.jsx
@@ -24,3 +24,17 @@ export const ResultContainer = styled.div`
   display: flex;
   gap: 10px;
 `;
+
+export const ListContainer = styled.ul`
+  width: 100%;
+
+  li {
+    display: flex;
+    background-color: #e7e7e7;
+    justify-content: space-between;
+    align-items: center;
+    padding: 6px 20px;
+    border-radius: 8px;
+    margin-bottom: 8px;
+  }
+`;
