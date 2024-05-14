c742ab16d6a7a3b866c28a7031e20e4f8ce5373f
DiegoCoura <diegogomes.10.9@gmail.com>
2024-05-13 23:20:53 -0300

    feat: change the mock data

--- src/mock-data/rooms.js
+++ src/mock-data/rooms.js
@@ -2,25 +2,31 @@ export const roomsDB = [
   {
     room: "A-107B",
     status: "livre",
+    users: []
   },
   {
     room: "B-207",
     status: "livre",
+    users: []
   },
   {
     room: "C-100B",
     status: "livre",
+    users: []
   },
   {
     room: "D-25B",
     status: "livre",
+    users: []
   },
   {
     room: "F-190",
     status: "livre",
+    users: []
   },
   {
     room: "B-35B",
     status: "livre",
+    users: []
   },
 ];
--- src/mock-data/roomsRequests.js
+++ src/mock-data/roomsRequests.js
@@ -1,1 +1,9 @@
-export const roomRequests = [];
+export const roomRequests = [
+  {
+    id: "A-107B",
+    room: "A-107B",
+    name: "Jussaro Cleber",
+    phone: "(11)9999-9999",
+    status: "pendente",
+  },
+];
