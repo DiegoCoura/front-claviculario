df472027c8a25a05284f89478842d9553739237a
DiegoCoura <diegogomes.10.9@gmail.com>
2024-05-13 23:22:49 -0300

    feat: add ClassRoomsStyled component

---
+++ src/pages/User/ClassRooms/ClassRoomsStyled.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const ClassRoomsContainer = styled.ul`
+  li {
+    display: flex;
+    background-color: #e7e7e7;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+    border-radius: 8px;
+    margin-bottom: 8px;
+  }
+`;
+
+export const Status = styled.div`
+  background-color: green;
+  border-radius: 8px;
+  color: #fff;
+  padding: 4px 10px;
+`;
