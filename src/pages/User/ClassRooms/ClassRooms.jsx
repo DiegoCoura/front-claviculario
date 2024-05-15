e4ebc1509ac9b129e9a3909133d378ec5fcc54d4
DiegoCoura <diegogomes.10.9@gmail.com>
2024-05-14 23:47:41 -0300

    feat: add buttons to access each class users list on the ClassRooms page

--- src/pages/User/ClassRooms/ClassRooms.jsx
+++ src/pages/User/ClassRooms/ClassRooms.jsx
@@ -1,28 +1,58 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../../../Context/UserContext";
 import { Users } from "../../../mock-data/Users";
-import { getRoom, getUser } from "../../../utils/getFunctions";
+import { getRoom, getUser } from "../../../utils/helperFunctions";
 import { roomsDB } from "../../../mock-data/rooms";
-import { ClassRoomsContainer, Status } from "./ClassRoomsStyled";
+import {
+  ClassRoomsContainer,
+  SingleRoomContainer,
+  Status,
+} from "./ClassRoomsStyled";
 
 export default function ClassRooms() {
   const { user } = useContext(UserContext);
+  const [currRoom, setCurrRoom] = useState("");
 
   const currUser = getUser(Users, user);
 
-  const currUsersRooms = currUser.classrooms.map((room) => {
+  const selectRoom = (e) => {
+    const tempRoom = getRoom(roomsDB, e.target.id);
+    setCurrRoom(tempRoom);
+  };
+
+  const currUserRooms = currUser.classrooms.map((room) => {
     let currRoom = getRoom(roomsDB, room);
     return (
-      <li key={room}>
+      <button key={room} id={room} onClick={(e) => selectRoom(e)}>
         <div>{currRoom.room}</div>
         <Status>{currRoom.status}</Status>
-      </li>
+      </button>
     );
   });
+  let currRoomUsersList
+  if (currRoom) {
+    currRoomUsersList = currRoom.users.map((person) => {
+      return (
+        <li key={person.email}>
+          <div>
+            {`${person.name} - ${person.phone}`}
+          </div>
+          <div>
+            <button>Remover</button>
+          </div>
+        </li>
+      );
+    });
+  }
 
   return (
     <>
-      <ClassRoomsContainer>{currUsersRooms}</ClassRoomsContainer>
+      {(currRoom && (
+        <SingleRoomContainer>
+          <h1>SALA: {currRoom.room}</h1>
+          {currRoomUsersList}
+        </SingleRoomContainer>
+      )) || <ClassRoomsContainer>{currUserRooms}</ClassRoomsContainer>}
     </>
   );
 }
--- src/pages/User/ClassRooms/ClassRoomsStyled.jsx
+++ src/pages/User/ClassRooms/ClassRoomsStyled.jsx
@@ -1,14 +1,29 @@
 import styled from "styled-components";
 
-export const ClassRoomsContainer = styled.ul`
-  li {
+export const ClassRoomsContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+
+  button {
     display: flex;
-    background-color: #e7e7e7;
-    justify-content: space-between;
+    flex-direction: column;
     align-items: center;
+    width: 120px;
+    cursor: pointer;
+    background-color: #e7e7e7;
     padding: 10px 20px;
+    border: 1px solid black;
     border-radius: 8px;
     margin-bottom: 8px;
+    font-size: 1rem;
+    transition: 0.2s ease-out;
+  }
+
+  button:hover {
+    transform: scale(1.05);
+  }
+  button > * {
+    pointer-events: none;
   }
 `;
 
@@ -17,4 +32,39 @@ export const Status = styled.div`
   border-radius: 8px;
   color: #fff;
   padding: 4px 10px;
+`;
+
+export const SingleRoomContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px 20px;
+
+  h1 {
+    margin: 1rem auto 2rem;
+  }
+
+  li {
+    display: flex;
+    background-color: #e7e7e7;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+    border-radius: 8px;
+    margin-bottom: 8px;
+    width: 100%;
+  }
+
+  button {
+    background-color: tomato;
+    border: none;
+    border-radius: 8px;
+    padding: 10px;
+    cursor: pointer;
+    transition: 0.2s ease-out;
+  }
+
+  button:hover {
+    transform: scale(1.05);
+  }
 `;
