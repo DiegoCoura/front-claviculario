93a75fe585dbbdcf2e74423ce65859cb5689c57a
DiegoCoura <diegogomes.10.9@gmail.com>
2024-05-14 23:42:35 -0300

    feat: add user to roomRequests mock data

--- src/mock-data/roomsRequests.js
+++ src/mock-data/roomsRequests.js
@@ -1,9 +1,20 @@
 export const roomRequests = [
   {
-    id: "A-107B",
+    id: "A-107B_jusarinho@email.com",
     room: "A-107B",
     name: "Jussaro Cleber",
+    role: "professor",
     phone: "(11)9999-9999",
+    email: "jusarinho@email.com",
+    status: "pendente",
+  },
+  {
+    id: "B-207_clovis@email.com",
+    room: "B-207",
+    name: "Clovis Ravazzi",
+    role: "admin",
+    phone: "(11)9999-9999",
+    email: "clovis@email.com",
     status: "pendente",
   },
 ];
